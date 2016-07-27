71dade4db2c7f79715ee259703cc30a7f2cf93d4
ZolayvarE <ZolayvarECU@gmail.com>
2016-07-27 16:34:16 -0700

    Set up queue and stack using Object.create()

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var returnObj = Object.create(queueMethods);
+  returnObj.storage = {};
+
+  return returnObj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function (input) {
+    var highestIndex = function (old) {
+      var indexArray = Object.keys(old.storage);
+      if (indexArray.length === 0) { return 0; }
+      return Math.max.apply(null, indexArray);
+    };
+    var index = highestIndex(this) + 1;
+    this.storage[index] = input;
+  },
+
+  dequeue: function () {
+    var lowestIndex = function (old) {
+      var indexArray = Object.keys(old.storage);
+      return Math.min.apply(null, indexArray);
+    };
+    var index = lowestIndex(this);
+    var temp = this.storage[index];
+    delete this.storage[index];
+    return temp;
+  },
+
+  size: function () { return Object.keys(this.storage).length; }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var returnObject = Object.create(stackMethods);
+  returnObject.storage = {};
+
+  return returnObject;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(input) {
+    var index = this.size();
+    this.storage[index] = input;
+  },
+  pop: function() {
+    var index = this.size() - 1;
+    var temp = this.storage[index];
+    delete this.storage[index];
+    return temp;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
