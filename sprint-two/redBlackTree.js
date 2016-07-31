e90e957b446fb757a786ce252a0b24f8e83700ee
ZolayvarE <ZolayvarECU@gmail.com>
2016-07-31 15:39:08 -0700

    Did some general clean up. rotating seems to work, but colors don't update afterwards

--- sprint-two/redBlackTree.js
+++ sprint-two/redBlackTree.js
@@ -5,79 +5,63 @@ var redBlackRoot = function (value) {
 var RnBtree = function (value, side, parent) {
   this.value = value;
   this.color = 'red';
-  this.left = null;
-  this.right = null;
+  this.left = { parent: this, value: null, color: 'black', side: 0 };
+  this.right = { parent: this, value: null, color: 'black', side: 1 };
   this.parent = parent;
   this.side = side;
   this.case1(this);
 };
 
-var RnB = RnBtree.prototype;
-
-RnBtree.prototype.getColor = function (node) {
-  if (node === undefined) { node = this; }
-  if (node === null) {
-    return 'black';
+RnBtree.prototype.getSibling = function () {
+  if (this.side) {
+    return this.parent.left;
   } else {
-    return node.color;
+    return this.parent.right;
   }
 };
 
-RnBtree.prototype.setColor = function (color, node) {
-  if (node === undefined) { node = this; }
-  if (node !== null) {
-    node.color = color;
-  }
-};
+var RnB = RnBtree.prototype;
 
 RnBtree.prototype.case1 = function (n) {
   if (n.parent === null) {
-    n.setColor('black');
+    n.color = 'black';
   }
 };
 
 RnBtree.prototype.case2 = function (n) {
-  if (RnB.getColor(n.parent) === 'black') {
+  if (n.parent.color === 'black') {
     return true;
   } else {
     return false;
   }
 };
 
-RnBtree.prototype.grabRoot = function (oldRoot) {
-  oldRoot = oldRoot.parent;
-};
-
 RnBtree.prototype.case3 = function (n) {
-  var parent = n.parent;
-  var grandParent = n.parent.parent;
-  if (parent.side) { 
-    var uncle = n.parent.parent.left;
-  } else {
-    var uncle = n.parent.parent.right;
-  }
-  if (RnB.getColor(parent) === 'red' && RnB.getColor(uncle) === 'red') {
-    RnB.setColor('black', parent);
-    RnB.setColor('black', uncle);
-    RnB.checkCases(grandParent);
+  if (n.parent !== null && n.parent.parent !== null) {
+    var parent = n.parent;
+    var grandParent = n.parent.parent;
+    var uncle = parent.getSibling();
+    if (parent.color === 'red' && uncle.color === 'red') {
+      parent.color = 'black';
+      uncle.color = 'black';
+      RnB.checkCases(grandParent);
+    }
   }
 };
 
 RnBtree.prototype.case4 = function (n) {
-  var parent = n.parent;
-  var grandParent = n.parent.parent;
-  if (parent.side) {
-    var uncle = grandParent.left;
-  } else {
-    var uncle = grandParent.right;
-  }
-  if (RnB.getColor(uncle) === 'black' && RnB.getColor(parent) === 'red') {
-    if (parent.side === 1 && n.side === 0) {
-      RnB.rotateRight(n);
-      RnB.checkCases(n.right);
-    } else if (parent.side === 0 && n.side === 1) {
-      RnB.rotateLeft(n);
-      RnB.checkCases(n.left);
+  if (n.parent !== null && n.parent.parent !== null) {
+    var parent = n.parent;
+    var grandParent = n.parent.parent;
+    var uncle = parent.getSibling();
+    if (parent.color === 'red' && uncle.color === 'black') {
+      if (n.side === 1 && parent.side === 0) {
+        RnB.rotateLeft(n);
+        RnB.checkCases(n.left);
+      } else if (n.side === 0 && parent.side === 1) {
+        RnB.rotateRight(n);
+        RnB.checkCases(n.right);
+      }
     }
   }
 };
@@ -85,21 +69,15 @@ RnBtree.prototype.case4 = function (n) {
 RnBtree.prototype.case5 = function (n) {
   if (n.parent !== null && n.parent.parent !== null) {
     var parent = n.parent;
-    var grandParent = n.parent.parent;
-    if (parent.side) {
-      var uncle = grandParent.left;
-    } else {
-      var uncle = grandParent.right;
-    }
-    if (RnB.getColor(uncle) === 'black' && RnB.getColor(parent) === 'red') {
-      if (parent.side === 1 && n.side === 1) {
+    var grandparent = n.parent.parent;
+    var uncle = parent.getSibling();
+    if (parent.color === 'red' && uncle.color === 'black') {
+      if (n.side === 1 && parent.side === 1) {
         RnB.rotateLeft(parent);
-        grandParent.setColor('red');
-        RnB.checkCases(parent);
-      } else if (parent.side === 0 && n.side === 0) {
+        RnB.checkCases(n.parent);
+      } else if (n.side === 0 && parent.side === 0) {
         RnB.rotateRight(parent);
-        grandParent.setColor('red');
-        RnB.checkCases(parent);
+        RnB.checkCases(n);
       }
     }
   }
@@ -120,7 +98,7 @@ RnBtree.prototype.insert = function (value) {
   var check = function (tree) {
     if (tree !== null) {
       if (value < tree.value) {
-        if (tree.left === null) {
+        if (tree.left.value === null) {
           var graft = new RnBtree(value, 0, tree);
           tree.left = graft;
           RnB.checkCases(graft);
@@ -128,7 +106,7 @@ RnBtree.prototype.insert = function (value) {
           check(tree.left);
         }
       } else if (value > tree.value) {
-        if (tree.right === null) {
+        if (tree.right.value === null) {
           var graft = new RnBtree(value, 1, tree);
           tree.right = graft;
           RnB.checkCases(graft);
@@ -146,7 +124,7 @@ RnBtree.prototype.contains = function (value) {
   var found = false;
 
   var check = function (tree) {
-    if (tree === null) { return; }
+    if (tree.value === null) { return; }
     if (tree.value === value) {
       found = true;
     } else if (value > tree.value) {
@@ -162,46 +140,48 @@ RnBtree.prototype.contains = function (value) {
 };
 
 RnBtree.prototype.rotateLeft = function (node) {
+  var savedNode = RnB.extend(new RnBtree(null, null, null), node);
+  var savedParent = RnB.extend(new RnBtree(null, null, null), node.parent);
   var savedLeft = node.left;
-  var parent = node.parent;
-  var grandParent = node.parent.parent;
-
-  node.side = node.parent.side;
-  node.left = node.parent;
-  node.parent = node.parent.parent;
-  node.left.side = 0;
-  node.left.right = savedLeft;
-  node.left.parent = node;
-  if (node.parent !== null) {
-    if (node.side) {
-      node.parent.right = node;
-    } else {
-      node.parent.left = node;
-    }
-  }
-};
-
-RnBtree.prototype.extend = function (obj1, obj2) {
-  if (obj1 === null) { obj1 = {}; }
-  if (obj2 === null) { return obj1 = null; }
-  for (var item in obj1) {
-    obj1[item] = obj2[item];
-  }
+  savedNode.parent = node.parent.parent;
+  savedNode.side = node.parent.side;
+  // Pair;
+  savedParent.right = savedLeft;
+  savedLeft.parent = savedParent;
+  // Pair;
+  savedNode.left = savedParent;
+  savedParent.parent = savedNode;
+  savedNode.right.parent = savedNode;
+  savedParent.left.parent = savedParent;
+  RnB.extend(node.parent, savedNode);
+  return savedNode;
 };
 
 RnBtree.prototype.rotateRight = function (node) {
+  var savedNode = RnB.extend(new RnBtree(null, null, null), node);
+  var savedParent = RnB.extend(new RnBtree(null, null, null), node.parent);
   var savedRight = node.right;
-  node.side = node.parent.side;
-  node.right = node.parent;
-  node.parent = node.parent.parent;
-  node.right.side = 1;
-  node.right.left = savedRight;
-  node.right.parent = node;
-  if (node.parent !== null) {
-    if (node.side) {
-      node.parent.right = node;
-    } else {
-      node.parent.left = node;
-    }
-  }
+  savedNode.parent = node.parent.parent;
+  savedNode.side = node.parent.side;
+  // Pair;
+  savedParent.left = savedRight;
+  savedRight.parent = savedParent;
+  // Pair;
+  savedNode.right = savedParent;
+  savedParent.parent = savedNode;
+  savedNode.left.parent = savedNode;
+  savedParent.right.parent = savedParent;
+  RnB.extend(node.parent, savedNode);
+  return savedNode;
+};
+
+RnBtree.prototype.extend = function (obj1, obj2) {
+  if (obj1 === null) { obj1 = new RnBtree(null, null, null); }
+  if (obj2 === null) { return obj1 = null; }
+  var items = ['value', 'color', 'left', 'right', 'parent', 'side'];
+  for (var i = 0; i < items.length; i++) {
+    obj1[items[i]] = obj2[items[i]];
+  }  
+
+  return obj1;
 };
